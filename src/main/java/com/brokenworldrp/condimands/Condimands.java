313b860b112f1bfdb91bcbe1e2a2434bdac53748
svmarin <sv.marin26@gmail.com>
2024-09-12 18:45:31 +0200

    initial commit

---
+++ src/main/java/com/brokenworldrp/condimands/Condimands.java
@@ -0,0 +1,19 @@
+package com.brokenworldrp.condimands;
+
+import com.brokenworldrp.condimands.commands.WipeTagsCommand;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Condimands extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        Bukkit.getPluginCommand("wipeTags").setExecutor(new WipeTagsCommand());
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
